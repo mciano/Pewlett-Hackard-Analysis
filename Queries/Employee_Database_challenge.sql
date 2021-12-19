3588a7f46d2dbb81ed74c801c375789cef802dec
Marcio <92833805+mciano@users.noreply.github.com>
2021-12-19 16:50:35 -0500

    update challenge query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,15 +1,16 @@
+-- Use Dictinct with Orderby to remove duplicate rows
 --Deliverable 1
-select e.emp_no, 
+Select e.emp_no, 
 	e.first_name, 
 	e.last_name, 
 	t.title, 
 	t.from_date, 
 	t.to_date
-into retirement_titles
-from employees as e
-inner join titles as t on (e.emp_no = t.emp_no)
+Into retirement_titles
+From employees as e
+Inner join titles as t on (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-order by e.emp_no;
+Order by e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -19,7 +20,7 @@ SELECT DISTINCT ON (emp_no) emp_no,
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
-SELECT * FROM unique_titles;
+
 -- number of employees retiring by title
 select count(title) as total, 
 	title
@@ -27,9 +28,11 @@ into retiring_titles
 from unique_titles
 group by title
 order by total desc; 
-SELECT * FROM retiring_titles;
+
+SELECT * FROM retirement_titles;
+
 --------------------------------------------------
---deliverable 2--
+--Deliverable 2
 select distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,de.from_date, de.to_date,t.title
 into mentor_elig
 from employees as e
@@ -38,5 +41,3 @@ inner join titles as t on (e.emp_no = t.emp_no)
 where de.to_date = ('9999-01-01')
 and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 order by e.emp_no;
-
-SELECT * FROM mentor_elig;
